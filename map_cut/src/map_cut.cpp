5f4e9f364308f49257fd709e428d33b5a2fd81c0
huang-yu-ta <eric5200eric5200@gmail.com>
2020-04-29 18:31:50 +0800

    update map_cut.cpp

--- map_cut/src/map_cut.cpp
+++ map_cut/src/map_cut.cpp
@@ -4,6 +4,7 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <stdio.h>
 
 
 main (int argc, char **argv)
@@ -15,15 +16,24 @@ main (int argc, char **argv)
     pcl::PointCloud<pcl::PointXYZ> cloud;
     sensor_msgs::PointCloud2 out;
 
+    int x, y; //current grid
+    x = y = 0;
+    char file_1[50], file_2[50], file_3[50], file_4[50], file_5[50];
+    sprintf(file_1,"/home/eric/Desktop/%dx%d.pcd",x ,y);
+    sprintf(file_2,"/home/eric/Desktop/%dx%d.pcd",x ,y+1);
+    sprintf(file_3,"/home/eric/Desktop/%dx%d.pcd",x ,y-1);
+    sprintf(file_4,"/home/eric/Desktop/%dx%d.pcd",x+1 ,y);
+    sprintf(file_5,"/home/eric/Desktop/%dx%d.pcd",x-1 ,y);
+
     ros::Rate r(100);
     while (nh.ok())
     {
         ros::spinOnce();
-        pcl::io::loadPCDFile("/home/eric/Desktop/c.pcd",cloud_1);
-        pcl::io::loadPCDFile("/home/eric/Desktop/f.pcd",cloud_2);
-        pcl::io::loadPCDFile("/home/eric/Desktop/b.pcd",cloud_3);
-        pcl::io::loadPCDFile("/home/eric/Desktop/l.pcd",cloud_4);
-        pcl::io::loadPCDFile("/home/eric/Desktop/r.pcd",cloud_5);
+        pcl::io::loadPCDFile(file_1,cloud_1);
+        pcl::io::loadPCDFile(file_2,cloud_2);
+        pcl::io::loadPCDFile(file_3,cloud_3);
+        pcl::io::loadPCDFile(file_4,cloud_4);
+        pcl::io::loadPCDFile(file_5,cloud_5);
         cloud = cloud_1 + cloud_2;
         cloud += cloud_3;
         cloud += cloud_4;

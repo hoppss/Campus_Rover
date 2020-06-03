1ef8ee49c133316216f2830f0c4a34c94f434692
huang-yu-ta <eric5200eric5200@gmail.com>
2020-06-03 11:42:13 +0800

    add param into map_combine

--- map_tool_standalone/map_combine/src/map_combine.cpp
+++ map_tool_standalone/map_combine/src/map_combine.cpp
@@ -1,21 +1,52 @@
 #include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <std_msgs/String.h>
+
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl_conversions/pcl_conversions.h>
+
 #include <stdio.h>
+
 #include <map_load/load.h>
 
-float dis;
-float pose_x, pose_y;
-std::string floors;
+float dis, leafs;
+float pose_x = 0.0, pose_y = 0.0, initial_pose_x, initial_pose_y;
+std::string dir, floors, c_status, m_status;
+char cstr[9][50];
+
+ros::Subscriber controller_sub;
 ros::Subscriber floor_sub;
-ros::Subscriber sub;
+ros::Subscriber ndt_sub;
+ros::Subscriber initial_pose;
+
 ros::Publisher point_pub;
+
 ros::ServiceClient client;
+
+pcl::VoxelGrid<pcl::PointXYZI> voxel;
+
+pcl::PointCloud<pcl::PointXYZI> cloud_n;
+pcl::PointCloud<pcl::PointXYZI> filter_cloud[9];
+
+sensor_msgs::PointCloud2 out;
+
+map_load::load srv;
+
+void getParameters(ros::NodeHandle nh_p)
+{
+    nh_p.param<float>("grid_distant", dis, 7.0);
+    nh_p.param<float>("leafs", leafs, 0.2);
+    nh_p.param<std::string>("dir", dir, "/home/eric/Desktop/map2/");
+}
+
+void controller_callback(const std_msgs::String& c)
+{
+    c_status = c.data;
+}
 
 void floor_callback(const std_msgs::String& f)
 {
@@ -28,65 +59,79 @@ void callback(const geometry_msgs::PoseStamped& p)
     pose_y = p.pose.position.y;
 }
 
+void pose_callback(const geometry_msgs::PoseWithCovarianceStamped& i)
+{
+    initial_pose_x = i.pose.pose.position.x;
+    initial_pose_y = i.pose.pose.position.y;
+}
+
 main (int argc, char **argv)
 {
     ros::init (argc, argv, "map_combine");
     ros::NodeHandle nh;
+    ros::NodeHandle nh_p("~");
     floor_sub = nh.subscribe("floor", 100, floor_callback);
-    sub = nh.subscribe("ndt_pose", 100, callback);
+    ndt_sub = nh.subscribe("ndt_pose", 100, callback);
+    controller_sub = nh.subscribe("controller", 100, controller_callback);
+    initial_pose = nh.subscribe("initialpose", 100, pose_callback);
     point_pub = nh.advertise<sensor_msgs::PointCloud2>("points_map", 100);
     client = nh.serviceClient<map_load::load>("map_load");
-    pcl::PointCloud<pcl::PointXYZI> cloud_1, cloud_2, cloud_3, cloud_4, cloud_5;
-    pcl::PointCloud<pcl::PointXYZI> cloud_6, cloud_7, cloud_8, cloud_9;
-    pcl::PointCloud<pcl::PointXYZI> cloud;
-    sensor_msgs::PointCloud2 out;
-    map_load::load srv;
-    
-    dis = 7.0;
+
+    getParameters(nh_p);
 
     ros::Rate r(100);
     while (nh.ok())
     {
         ros::spinOnce();
-        srv.request.x = pose_x;
-        srv.request.y = pose_y;
-        srv.request.dis = dis;
+        pcl::PointCloud<pcl::PointXYZI> cloud;
+        if (c_status == "elevator")
+        {
+            srv.request.x = initial_pose_x;
+            srv.request.y = initial_pose_y;
+            srv.request.dis = dis;
+            client.call(srv);
 
-        client.call(srv);
+            std::string str[9] = {srv.response.a, srv.response.b, srv.response.c, 
+            srv.response.d, srv.response.e, srv.response.f, 
+            srv.response.g, srv.response.h, srv.response.i};
 
-        std::string str[9] = {srv.response.a, srv.response.b, srv.response.c, 
-        srv.response.d, srv.response.e, srv.response.f, 
-        srv.response.g, srv.response.h, srv.response.i};
-        char cstr[9][50];
+            sprintf(cstr[0], (dir + "%s").c_str(), (floors + "/" +str[0]).c_str());
+            pcl::io::loadPCDFile(cstr[0],cloud_n);
+
+            pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
+            *cloud_ptr = cloud_n;
+            voxel.setInputCloud(cloud_ptr);
+            voxel.setLeafSize(leafs, leafs, leafs);
+            voxel.filter(filter_cloud[0]);
+
+            cloud = filter_cloud[0];
+        }
+        else if (c_status == "planner")
+        {
+            srv.request.x = pose_x;
+            srv.request.y = pose_y;
+            srv.request.dis = dis;
+            client.call(srv);
+
+            std::string str[9] = {srv.response.a, srv.response.b, srv.response.c, 
+            srv.response.d, srv.response.e, srv.response.f, 
+            srv.response.g, srv.response.h, srv.response.i};
+
+            for (int i = 0; i < 9; i++)
+            {
+                sprintf(cstr[i], (dir + "%s").c_str(), (floors + "/" +str[i]).c_str());
+                pcl::io::loadPCDFile(cstr[i],cloud_n);
+
+                pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
+                *cloud_ptr = cloud_n;
+                voxel.setInputCloud(cloud_ptr);
+                voxel.setLeafSize(leafs, leafs, leafs);
+                voxel.filter(filter_cloud[i]);
+                
+                cloud += filter_cloud[i];
+            }
+        }
         
-        sprintf(cstr[0],"/home/eric/Desktop/map2/%s",(floors + "/" +str[0]).c_str());
-        sprintf(cstr[1],"/home/eric/Desktop/map2/%s",(floors + "/" +str[1]).c_str());
-        sprintf(cstr[2],"/home/eric/Desktop/map2/%s",(floors + "/" +str[2]).c_str());
-        sprintf(cstr[3],"/home/eric/Desktop/map2/%s",(floors + "/" +str[3]).c_str());
-        sprintf(cstr[4],"/home/eric/Desktop/map2/%s",(floors + "/" +str[4]).c_str());
-        sprintf(cstr[5],"/home/eric/Desktop/map2/%s",(floors + "/" +str[5]).c_str());
-        sprintf(cstr[6],"/home/eric/Desktop/map2/%s",(floors + "/" +str[6]).c_str());
-        sprintf(cstr[7],"/home/eric/Desktop/map2/%s",(floors + "/" +str[7]).c_str());
-        sprintf(cstr[8],"/home/eric/Desktop/map2/%s",(floors + "/" +str[8]).c_str());
-
-        pcl::io::loadPCDFile(cstr[0],cloud_1);
-        pcl::io::loadPCDFile(cstr[1],cloud_2);
-        pcl::io::loadPCDFile(cstr[2],cloud_3);
-        pcl::io::loadPCDFile(cstr[3],cloud_4);
-        pcl::io::loadPCDFile(cstr[4],cloud_5);
-        pcl::io::loadPCDFile(cstr[5],cloud_6);
-        pcl::io::loadPCDFile(cstr[6],cloud_7);
-        pcl::io::loadPCDFile(cstr[7],cloud_8);
-        pcl::io::loadPCDFile(cstr[8],cloud_9);        
-        cloud = cloud_1 + cloud_2;
-        cloud += cloud_3;
-        cloud += cloud_4;
-        cloud += cloud_5;
-        cloud += cloud_6;
-        cloud += cloud_7;
-        cloud += cloud_8;
-        cloud += cloud_9;
-
         pcl::toROSMsg(cloud,out);
         out.header.stamp = ros::Time::now();
         out.header.frame_id = "map";
